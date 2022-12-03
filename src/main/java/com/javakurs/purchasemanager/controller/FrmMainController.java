f542c96c725fded676db5ae2713aade5dc76b108
Adik08 <adik08.eamil@gmail.com>
2022-12-03 16:19:54 +0100

    TSK-5 Formatka główna - wygląd GUI

--- src/main/java/com/javakurs/purchasemanager/controller/FrmMainController.java
+++ src/main/java/com/javakurs/purchasemanager/controller/FrmMainController.java
@@ -1,4 +1,20 @@
 package com.javakurs.purchasemanager.controller;
 
+import com.javakurs.purchasemanager.enums.WindowType;
+import com.javakurs.purchasemanager.helper.WindowHelper;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class FrmMainController {
+
+    public Button btnLogOut;
+
+    public void btnLogOutClicked(ActionEvent actionEvent) {
+
+    }
+
+    public void btnLeaveClicked(ActionEvent actionEvent) {
+
+    }
 }
